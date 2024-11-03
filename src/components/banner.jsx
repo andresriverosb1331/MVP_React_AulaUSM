185fd09b57cb19de2271a863263f369fe25c22f9
Andrés riveros bueno <andres.riveros1331@gmail.com>
2024-11-03 00:31:31 -0300

    Actualización 3 de AULA(homepage-banner-cursos)

---
+++ src/components/banner.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import img1 from '../assets/img11.png'
+import img2 from '../assets/light-off.png'
+const BannerCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Array of images (replace with your own image URLs)
+  const images = [
+    img1,img2
+  ];
+
+  // Function to go to the next slide
+  const nextSlide = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
+
+  // Function to go to the previous slide
+  const prevSlide = () => {
+    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+  };
+
+  // useEffect to set up the timer for auto-slide
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 3000); // Change image every 3 seconds
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, [currentIndex]);
+
+  return (
+    <div className="banner-carousel">
+      <button onClick={prevSlide} className="carousel-button">◀</button>
+      <div className="banner-image">
+        <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      </div>
+      <button onClick={nextSlide} className="carousel-button">▶</button>
+    </div>
+  );
+};
+
+export default BannerCarousel;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
     return (
       <div className='contenedor-ramos'>
         <nav>
-            <div className="nav nav-tabs" id="nav-tab" role="tablist">Mis Cursos
+            <div className="nav nav-tabs" id="nav-tab" role="tablist">Mis Cursos:
                 <NavLink to="/mis-cursos" className="nav-link" id="nav-home-tab" role="tab" aria-selected="true">
                     2024-02
                 </NavLink>
@@ -18,27 +18,32 @@ const Card = () => {
                 </NavLink>
             </div>
         </nav>
-    
-        <div className='card'>
-          <div className='rectangle1'></div>
-          <div className='ramo'>
-            Diseño de interfaces usuarias | Paralelos: 200/201/202 (2024-02)(INF322)
+        
+        <div className='Card1'>
+          <div className='info-ramo'>
+            <div className='rectangle1'>
+              <div className='rectangle2'>
+                <div className='carrera'>Informática</div>
+              </div>
+            </div>
+            <div className='ramo'>
+              Diseño de interfaces usuarias | Paralelos: 200/201/202 (2024-02)(INF322)
+            </div>
+            </div>
+          <div className='alertas-ramo'> Alertas
+            <div className='descripcion-alerta'>
+              Entrega 1 proyecto | 07/09/2024
+            </div>
+            <div className='descripcion-alerta'>
+              Entrega 2 proyecto | 05/11/2024
+            </div>
+            <div className='descripcion-alerta'>
+              Entrega 3 proyecto | 27/11/2024
+            </div>
           </div>
-          <div className='group1'>
-            <div className='rectangle2'></div>
-            <div className='carrera'>Informática</div>
           </div>
-        </div>
-        <div className='card'>
-          <div className='rectangle1'></div>
-          <div className='ramo'>
-            Computación científica | Paralelos: 200/201/202 (2024-02)(INF285)
-          </div>
-          <div className='group1'>
-            <div className='rectangle2'></div>
-            <div className='carrera'>Informática</div>
-          </div>
-        </div>
+
+
       </div>
       
       
